dd08f0405b0ecb6401432bbc1cfbc03086f49600
Jason Mcaffee <jason.mcaffee@insidesales.com>
2017-05-28 20:40:33 -0600

    initial commit

--- goreddit.go
+++ goreddit.go
@@ -1,11 +1,11 @@
-package goreddit
-
-import "fmt"
-
-func init(){
-	fmt.Println("hi")
-}
-
-func main(){
-	fmt.Println("goreddit main")
-}
+//package goreddit
+//
+//import "fmt"
+//
+//func init(){
+//	fmt.Println("hi")
+//}
+//
+//func main(){
+//	fmt.Println("goreddit main")
+//}
